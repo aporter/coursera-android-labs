9941c0888235f0a2ae61c2a12b285e9c4a9f5eb2
b40153 <Jonatan_Dellagostin@banrisul.com.br>
2015-04-22 08:15:44 -0300

    Finalizando lab3b - Fragments

--- Permissions/Lab3b_PermissionsLab/src/course/labs/permissionslab/BookmarksActivity.java
+++ Permissions/Lab3b_PermissionsLab/src/course/labs/permissionslab/BookmarksActivity.java
@@ -79,6 +79,8 @@ public class BookmarksActivity extends Activity {
 		Log.i(TAG, "Entered startGoToDangerousActivity()");
 
 		// TODO - Start the GoToDangerousActivity
+		Intent intent = new Intent(this, GoToDangerousActivity.class);
+		startActivity(intent);
 		
 
 	}
