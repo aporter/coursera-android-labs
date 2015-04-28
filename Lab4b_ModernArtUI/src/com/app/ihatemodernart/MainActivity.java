8ec836182f0740a1ffdc96128ebe03382ca2f518
Jon <jdellagostin@gmail.com>
2015-04-28 05:32:31 -0300

    Finalizando o projeto final do curso :)

--- Lab4b_ModernArtUI/src/com/app/ihatemodernart/MainActivity.java
+++ Lab4b_ModernArtUI/src/com/app/ihatemodernart/MainActivity.java
@@ -1,154 +1,135 @@
 package com.app.ihatemodernart;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.SystemClock;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.app.DialogFragment;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.AlertDialog;
 import android.graphics.Color;
-import android.graphics.PorterDuff;
+import android.content.DialogInterface;
+import android.content.Intent;
 
 public class MainActivity extends ActionBarActivity {
 
 	// IDs for menu items
-	private static final int MENU_DELETE = Menu.FIRST;
-	private static final int LAUNCH_DIALOG = Menu.FIRST + 1;
-	static private final String mainTitle = "Modern Art UI";
-	static private final String seekBarStartTextView1 = this.getString(R.string.seekBarStartTextView1);
-	static private final String seekBarStopTextView1 = this.getString(R.string.seekBarStopTextView1);
+	private static final int DECREMENT = 0;
+	private static final int INCREMENT = 1;
+	private static final int LAUNCH_DIALOG = Menu.FIRST;
+	private static final String mainTitle = "Modern Art UI";
+	private static final String URL = "http://www.moma.org";
+	private final String initialColorTextView1 = "330000";
+	private final String initialColorTextView2 = "33ff77";
+	private final String initialColorTextView3 = "ffffff";
+	private final String initialColorTextView4 = "cc0022";
+	private final String initialColorTextView5 = "808080";
+	private final String initialColorTextView6 = "55ffff";
+	private final String initialColorTextView7 = "66eeff";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.activity_main);
 		setTitle(mainTitle);
-		seekBar = (SeekBar) findViewById(R.id.seekBar);
-		//ListAdapter mAdapter = new BaseAdapter(getApplicationContext());
-		//		volumeControl = (SeekBar) findViewById(R.id.volume_bar);
-		//
+		TextView textView1 = (TextView) findViewById(R.id.textView1);
+		setBackGroundColor(initialColorTextView1, textView1, 0, DECREMENT);
+		TextView textView2 = (TextView) findViewById(R.id.textView2);
+		setBackGroundColor(initialColorTextView2, textView2, 0, INCREMENT);
+		TextView textView3 = (TextView) findViewById(R.id.textView3);
+		setBackGroundColor(initialColorTextView3, textView3, 0, DECREMENT);
+		TextView textView4 = (TextView) findViewById(R.id.textView4);
+		setBackGroundColor(initialColorTextView4, textView4, 0, DECREMENT);
+		TextView textView5 = (TextView) findViewById(R.id.textView5);
+		setBackGroundColor(initialColorTextView5, textView5, 0, DECREMENT);
+		TextView textView6 = (TextView) findViewById(R.id.textView6);
+		setBackGroundColor(initialColorTextView6, textView6, 0, DECREMENT);
+		TextView textView7 = (TextView) findViewById(R.id.textView7);
+		setBackGroundColor(initialColorTextView7, textView7, 0, DECREMENT);
+		SeekBar seekBar = (SeekBar) findViewById(R.id.slide);
+		
 		seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
-			int progressChanged = 0;
 			@Override
 			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser){
-				Log.d("DEBUG", "Progress is: "+progress);
-				textView1 = (TextView) view.findViewById(R.id.textView1);
-				setBackGroundColor(seekBarStartTextView1, progress, textView);
-				textView2 = (TextView) view.findViewById(R.id.textView2);
-				setBackGroundColor(Color.GREEN, textView);
-				textView3 = (TextView) view.findViewById(R.id.textView3);
-				setBackGroundColor(Color.GREEN, textView);
-				textView4 = (TextView) view.findViewById(R.id.textView4);
-				setBackGroundColor(Color.GREEN, textView);
-				textView5 = (TextView) view.findViewById(R.id.textView5);
-				setBackGroundColor(Color.GREEN, textView);
-				textView5 = (TextView) view.findViewById(R.id.textView6);
-				setBackGroundColor(Color.GREEN, textView);
-				
+				TextView textView1 = (TextView) findViewById(R.id.textView1);
+				setBackGroundColor(initialColorTextView1, textView1, progress, DECREMENT);
+				TextView textView2 = (TextView) findViewById(R.id.textView2);
+				setBackGroundColor(initialColorTextView2, textView2, progress, INCREMENT);
+				TextView textView4 = (TextView) findViewById(R.id.textView4);
+				setBackGroundColor(initialColorTextView4, textView4, progress, DECREMENT);
+				TextView textView6 = (TextView) findViewById(R.id.textView6);
+				setBackGroundColor(initialColorTextView6, textView6, progress, DECREMENT);
+				TextView textView7 = (TextView) findViewById(R.id.textView7);
+				setBackGroundColor(initialColorTextView7, textView7, progress, DECREMENT);
 			}
-			//@Override
-			//public void onStartTrackingTouch(SeekBar seekBar) {
-			//	// TODO Auto-generated method stub
-			//}
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+			}
 			@Override
 			public void onStopTrackingTouch(SeekBar seekBar) {
-				//Toast.makeText(SeekbarActivity.this,"seek bar progress:"+progressChanged, 
-				//Toast.LENGTH_SHORT).show();
-				textView1 = (TextView) view.findViewById(R.id.textView1);
-				setBackGroundColor(seekBarStopTextView1, 0,  textView);
-				textView2 = (TextView) view.findViewById(R.id.textView2);
-				setBackGroundColor(Color.WHITE, textView);
-				textView3 = (TextView) view.findViewById(R.id.textView3);
-				setBackGroundColor(Color.WHITE, textView);
-				textView4 = (TextView) view.findViewById(R.id.textView4);
-				setBackGroundColor(Color.WHITE, textView);
-				textView5 = (TextView) view.findViewById(R.id.textView5);
-				setBackGroundColor(Color.WHITE, textView);
-				textView5 = (TextView) view.findViewById(R.id.textView6);
-				setBackGroundColor(Color.WHITE, textView);
 			}
 
-			private void setBackGroundColor (String colorHexString, View view, int colorOffset, String colorFlag) {
-			//private void setBackGroundColor (int color, View view) {
-				int colorDecimal = Integer.parseInt(colorHexString, 16);
-				if (colorFlag == DECREMENT ) {
-					int newColor = colorDecimal - colorOffset;
-				} else if (colorFlag == INCREMENT) {
-					int newColor = colorDecimal+colorOffset;
-				}
-				String colorCode = String.format("#%06x", newColor);
-				view.getBackground().setColorFilter(colorCode, PorterDuff.Mode.DARKEN);
-			}
-		}
+		});
+
 	}
-
-		SystemClock.sleep(3000);
-		setBackGroundColor(Color.CYAN, getWindow().getDecorView());
+	public void setBackGroundColor (String colorHexString, View view, int colorOffset, int colorFlag) {
+		int newColor = 0;
+		int colorDecimal = Integer.parseInt(colorHexString, 16);
+		if (colorFlag == DECREMENT ) {
+			newColor = colorDecimal - colorOffset*256;
+		} else if (colorFlag == INCREMENT) {
+			newColor = colorDecimal+colorOffset*256;
+		}
+		String colorCode = String.format("#%06x", newColor);
+		Log.d("DEBUG","Color code is "+colorCode);
+		view.setBackgroundColor(Color.parseColor(colorCode));
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		//getMenuInflater().inflate(R.menu.main, menu);
 		super.onCreateOptionsMenu(menu);
-		menu.add(Menu.NONE, MENU_DELETE, Menu.NONE, "Delete all");
-		menu.add(Menu.NONE, LAUNCH_DIALOG, Menu.NONE, "Dump to log");
+		menu.add(Menu.NONE, LAUNCH_DIALOG, Menu.NONE, "More information");
 		return true;
 	}
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-		case MENU_DELETE:
-			return true;
 		case LAUNCH_DIALOG:
-			DialogFragment momaDialog = new MomaDialogFragment();
-			momaDialog.show(getSupportFragmentManager(), "moma");
-			//MomaDialogFragment momaDialog = new MomaDialogFragment(context);
-			//momaDialog.show();
+			AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+			alertDialogBuilder.setTitle("Click bellow to learn more about Modern Art!");
+			// set positive button: Yes message
+			alertDialogBuilder.setPositiveButton("Visit the MOMA site now!",new DialogInterface.OnClickListener() {
+			       public void onClick(DialogInterface dialog,int id) {
+			           // go to a new activity of the app
+			    	   Uri webpage = Uri.parse(URL);
+			    	   Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+			    	   if (intent.resolveActivity(getPackageManager()) != null) {
+			    	   	startActivity(intent);
+			    	   }
+			       }
+			     });
+			// set negative button: No message
+			alertDialogBuilder.setNegativeButton("Not Now",new DialogInterface.OnClickListener() {
+			       public void onClick(DialogInterface dialog,int id) {
+			           // cancel the alert box and put a Toast to the user
+			           dialog.cancel();
+			           Toast.makeText(getApplicationContext(), "Thanks! Maybe next time?",
+			                   Toast.LENGTH_LONG).show();
+			       }
+			   });
+			 
+			AlertDialog alertDialog = alertDialogBuilder.create();
+			// show alert
+			alertDialog.show();
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
-	}
+		}
 
-	class MomaDialogFragment extends DialogFragment {
-		@Override
-		public Dialog onCreateDialog(Bundle savedInstanceState) {
-			// Use the Builder class for convenient dialog construction
-			//final Dialog dialog = new Dialog(context);
-			setContentView(R.layout.dialog_fragment);
-			setMessage(R.string.dialog_fragment_msg);
-			final Button dismissDialogButton = (Button) findViewById(R.id.dismiss_dialog_button);
-			dismissDialogButton.setTitle(R.string.dismiss_dialog_button_msg);
-			dismissDialogButton.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					// TODO - Indicate result and finish
-					//setResult(RESULT_CANCELED);
-					//finish();
-					v.dismiss();
-				}
-			final Button openWebPageButton = (Button) findViewById(R.id.openPageButton);
-			openWebPageButton.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					// TODO - Indicate result and finish
-					openWebPage(WEB_PAGE);
-					//setResult(RESULT_CANCELED);
-					finish();
-				}
-			});
-			public void openWebPage(String url) {
-				Uri webpage = Uri.parse(url);
-				Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
-				if (intent.resolveActivity(getPackageManager()) != null) {
-					startActivity(intent);
-				}
-			}
 	}
-
 }
